1b5cc59424f36f2cd4c4d79eaf730437f7320525
bokkle <mitch.r_@hotmail.com>
2023-07-13 19:47:51 -0400

    updated

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// 28 DAYS //
-
-const calculateNumerology = (date) => {
-  // Remove the slashes from the date string and convert it to a number
+const calculateNumerology = (date, targetValue) => {
   const numericDate = Number(date.replace(/\//g, ""));
-
-  // Calculate the sum of the digits in the numericDate
   let sum = 0;
   String(numericDate)
     .split("")
@@ -12,8 +7,13 @@ const calculateNumerology = (date) => {
       sum += Number(digit);
     });
 
-  // Reduce the sum to a single digit, unless it is 11, 22, 33, or 28
-  while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33 && sum !== 28) {
+  while (
+    sum > 9 &&
+    sum !== 11 &&
+    sum !== 22 &&
+    sum !== 33 &&
+    sum !== targetValue
+  ) {
     sum = String(sum)
       .split("")
       .reduce((acc, digit) => acc + Number(digit), 0);
@@ -22,82 +22,34 @@ const calculateNumerology = (date) => {
   return sum;
 };
 
-function findDaysAddingUpTo28(startYear, endYear) {
-  const daysAddingUpTo28 = [];
+function findDaysAddingUpToDate(startYear, endYear, targetValue) {
+  const daysAddingUpToDate = [];
 
-  // Iterate through each day of the specified years
   for (let year = startYear; year <= endYear; year++) {
     for (let month = 1; month <= 12; month++) {
       const daysInMonth = new Date(year, month, 0).getDate();
 
       for (let day = 1; day <= daysInMonth; day++) {
         const date = `${month}/${day}/${year}`;
-        const numerologyValue = calculateNumerology(date);
+        const numerologyValue = calculateNumerology(date, targetValue);
 
-        if (numerologyValue === 28) {
-          daysAddingUpTo28.push(date);
+        if (numerologyValue === targetValue) {
+          daysAddingUpToDate.push(date);
         }
       }
     }
   }
 
-  return daysAddingUpTo28;
+  return daysAddingUpToDate;
 }
 
 const startYear = 2023;
 const endYear = 2024;
-const daysAddingUpTo28 = findDaysAddingUpTo28(startYear, endYear);
+const daysAddingUpTo28 = findDaysAddingUpToDate(startYear, endYear, 28);
+const daysAddingUpTo8 = findDaysAddingUpToDate(startYear, endYear, 8);
 
 console.log("Days adding up to 28:");
 console.log(daysAddingUpTo28);
 
-// EIGHT DAYS //
-
-function calculateNumerologyForEight(date) {
-  const numericDate = Number(date.replace(/\//g, ""));
-  let sum = 0;
-  String(numericDate)
-    .split("")
-    .forEach((digit) => {
-      sum += Number(digit);
-    });
-
-  while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33 && sum !== 8) {
-    sum = String(sum)
-      .split("")
-      .reduce((acc, digit) => acc + Number(digit), 0);
-  }
-
-  return sum;
-}
-
-function findDaysAddingUpToEight(startYear, endYear) {
-  const daysAddingUpToEight = [];
-
-  for (let year = startYear; year <= endYear; year++) {
-    for (let month = 1; month <= 12; month++) {
-      const daysInMonth = new Date(year, month, 0).getDate();
-
-      for (let day = 1; day <= daysInMonth; day++) {
-        const date = `${month}/${day}/${year}`;
-        const numerologyValue = calculateNumerologyForEight(date);
-
-        if (numerologyValue === 8) {
-          daysAddingUpToEight.push(date);
-        }
-      }
-    }
-  }
-
-  return daysAddingUpToEight;
-}
-
-const startYearEight = 2023;
-const endYearEight = 2024;
-const daysAddingUpToEight = findDaysAddingUpToEight(
-  startYearEight,
-  endYearEight
-);
-
 console.log("Days adding up to 8:");
-console.log(daysAddingUpToEight);
+console.log(daysAddingUpTo8);
